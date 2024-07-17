f4be3d5a7b97e7a741ff397dbb84ab57a09fa2a2
matheusosan <matheussantanaoli@gmail.com>
2024-07-17 20:17:30 -0300

    feat: create appointment repository

---
+++ demo/src/main/java/com/spring_app/demo/repositories/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.spring_app.demo.repositories;
+
+import com.spring_app.demo.entities.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
