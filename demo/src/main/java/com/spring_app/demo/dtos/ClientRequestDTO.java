8bc9410704e6594d74fc69818f95c53a9e4f33af
matheusosan <matheussantanaoli@gmail.com>
2024-07-17 20:18:06 -0300

    feat: create client dto

---
+++ demo/src/main/java/com/spring_app/demo/dtos/ClientRequestDTO.java
@@ -0,0 +1,25 @@
+package com.spring_app.demo.dtos;
+
+public class ClientRequestDTO {
+
+    String name;
+    String cpf;
+    String email;
+    String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
